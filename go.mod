116d484795a64839162e47ef5dde5da89d00691e
mkumail <mo_kumail@mcafee.com>
2022-05-08 17:49:48 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-bookStore
+
+go 1.17
---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,14 @@
+package controllers
+
+import (
+	"github.com/kumail-sys/go-bookStore/pkg/models"
+	"net/http"
+)
+
+var NewBook models.Book
+
+func GetBook(w *http.ResponseWriter, r *http.Request) {
+	//newBooks := models.GetBook()
+	//res, _ := json.Marshal(newBooks)
+	
+}
